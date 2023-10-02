ed68ddca605b4708f13f912c7ebf7674bf0ac3ec
Jose David Parra Uribe <josedavidparrauribe@gmail.com>
2023-10-02 08:18:20 -0500

    se borra todo y se pone un solo boton

--- curso C# git hub/Form1.Designer.cs
+++ curso C# git hub/Form1.Designer.cs
@@ -29,91 +29,30 @@
         private void InitializeComponent()
         {
             button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            button4 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(378, 133);
+            button1.Location = new Point(293, 71);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(340, 151);
             button1.TabIndex = 0;
-            button1.Text = "lo lograste";
+            button1.Text = "eres el mejor";
             button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(113, 172);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "si puedes";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(317, 247);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 2;
-            button3.Text = "maldicionnnnn";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(200, 282);
-            label1.Name = "label1";
-            label1.Size = new Size(69, 15);
-            label1.TabIndex = 3;
-            label1.Text = "no te rindas";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(595, 244);
-            label2.Name = "label2";
-            label2.Size = new Size(153, 15);
-            label2.TabIndex = 4;
-            label2.Text = "se agrega un segundo texto";
-            // 
-            // button4
-            // 
-            button4.Location = new Point(538, 109);
-            button4.Name = "button4";
-            button4.Size = new Size(112, 51);
-            button4.TabIndex = 5;
-            button4.Text = "por fin";
-            button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button4);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(button3);
-            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
-        private Button button2;
-        private Button button3;
-        private Label label1;
-        private Label label2;
-        private Button button4;
     }
 }
